244f158d3ba393d955a34a89f262a5545ee3950b
Ao Lu <127536391+laleoarrow@users.noreply.github.com>
2026-07-31 03:13:19 +0000

    chore(cask): update idata to v0.2.13

--- Casks/idata.rb
+++ Casks/idata.rb
@@ -1,6 +1,6 @@
 cask "idata" do
-  version "0.2.12"
-  sha256 "48a30da78557a3b059bafd98345deef4b907c245b8a6ab20095f1f48391c7c0a"
+  version "0.2.13"
+  sha256 "04229f6b56209aa4f255ff4d0eb1af279139b2d39a0be0230946131e0e8a3ebf"
 
   url "https://github.com/laleoarrow/iData/releases/download/v#{version}/iData-v#{version}-macos-universal.zip"
   name "iData"
