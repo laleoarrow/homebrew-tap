f3ea0be34f4778154bee0d2e08e860bbad68504f
Ao Lu <luao@stu.cqmu.edu.cn>
2026-08-10 03:25:17 +0800

    Add Wattson v3.0.0 cask

---
+++ Casks/wattson.rb
@@ -0,0 +1,36 @@
+cask "wattson" do
+  version "3.0.0"
+  sha256 "e3311882a56e2c00fae4222464cd0911ac1564d4ce60f003b7213df5febfd9bb"
+
+  url "https://github.com/laleoarrow/battery-monitor/releases/download/v#{version}/Wattson-v#{version}-macos-universal.pkg",
+      verified: "github.com/laleoarrow/battery-monitor/"
+  name "Wattson"
+  desc "Native macOS menu-bar power-flow monitor"
+  homepage "https://github.com/laleoarrow/battery-monitor"
+
+  depends_on macos: ">= :monterey"
+
+  pkg "Wattson-v#{version}-macos-universal.pkg"
+
+  uninstall launchctl: [
+              "com.leoarrow.wattson.helper",
+              "com.leoarrow.wattson.login",
+            ],
+            quit:      "com.leoarrow.wattson",
+            pkgutil:   "com.leoarrow.wattson.pkg",
+            delete:    [
+              "/Applications/Wattson.app",
+              "/Library/LaunchDaemons/com.leoarrow.wattson.helper.plist",
+              "/Library/PrivilegedHelperTools/com.leoarrow.wattson.helper",
+              "/var/run/wattson-helper.sock",
+              "~/Library/LaunchAgents/com.leoarrow.wattson.login.plist",
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/Wattson",
+    "~/Library/Caches/com.leoarrow.wattson",
+    "~/Library/Containers/com.leoarrow.wattson",
+    "~/Library/Preferences/com.leoarrow.wattson.plist",
+    "~/Library/Saved Application State/com.leoarrow.wattson.savedState",
+  ]
+end
